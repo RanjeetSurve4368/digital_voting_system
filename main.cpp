8eda1f1e238420d6c8d971072efc37a216546c1e
RanjeetSurve4368 <169068143+RanjeetSurve4368@users.noreply.github.com>
2025-05-02 00:40:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,29 +23,39 @@ struct aDetails{
 void voterlogin(){
   
 }
+
+
+
+class Admin{
 vector<aDetails> Admins={
 {1,"Ranjeet",1111},
 {2,"God",2222} };
 
-void adminlogin(){
-  int Enterid, Enterpassword;
-  string Entername;
-  cout<<"Enter your name: "<<endl;
-  cin>>Entername;
+ public: 
+    int id,password;
+    string name;
 
-  cout<<"Enter your id: "<<endl;
-  cin>>Enterid;
+    // void Admin(int i,int pass,string n){ 
+    //  id=i;
+    //  password=pass;
+    //  name=n;
 
-  cout<<"Enter your password: "<<endl;
-  cin>>Enterpassword;
-
-  for(auto& admin: admins){
-    if(admin.id==Enterid && admin.name==Entername && admin.password==Enterpassword){
-      cout<<"Welcome "<<Entername<< endl;
-      cout<<"Please choose what you want to do from below options";
-      cout<<"1.Add new voter"<<endl<<"2.Add new candidate"<<endl<<"3.Check votes of Candidates"<<endl;
+    void getinput(){
+      cout<<"Enter your name: "<<endl;
+      cin>>name;
+      cout<<"Enter your id: "<<endl;
+      cin>>id;
+      cout<<"Enter your password: "<<endl;
+      cin>>password;
+    }
+    bool loginCheck(int id, int password, string name){
+     for(&auto admin: Admins){
+      if(admin.id=id && admin.password=password && admin.name=name){
+      cout<<"Welcome "<<name<<" to make changes";
+     }
+    }
       
-}
+};
 void login(){
   int login1;
   cout<<"If you want to login as voter please enter 1 and if you want to login as admin please enter 0"<<endl;
