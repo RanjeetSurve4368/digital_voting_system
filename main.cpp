37a806bb9001bc6c622cdc8e4da9094e000d6aac
RanjeetSurve4368 <169068143+RanjeetSurve4368@users.noreply.github.com>
2025-05-02 18:04:53 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ofstream>
 using namespace std;
 struct vDetails{
  int id;
@@ -32,8 +33,8 @@ vector<aDetails> Admins={
 {2,"God",2222} };
 
  public: 
-    int id,password;
-    string name;
+    int Eid,Epassword;
+    string Ename;
 
     // void Admin(int i,int pass,string n){ 
     //  id=i;
@@ -42,18 +43,73 @@ vector<aDetails> Admins={
 
     void getinput(){
       cout<<"Enter your name: "<<endl;
-      cin>>name;
+      cin>>Ename;
       cout<<"Enter your id: "<<endl;
-      cin>>id;
+      cin>>Eid;
       cout<<"Enter your password: "<<endl;
-      cin>>password;
+      cin>>Epassword;
+      loginCheck(Eid,Epassword,Ename);
     }
     bool loginCheck(int id, int password, string name){
      for(&auto admin: Admins){
-      if(admin.id=id && admin.password=password && admin.name=name){
-      cout<<"Welcome "<<name<<" to make changes";
+      if(admin.id=Eid && admin.password=Epassword && admin.name=Ename){
+      cout<<"Welcome "<<name<<" to make changes"<<endl;
+      cout<<"Please Choose options below "<<endl;
+      cout<<"1.Add Voter"<<endl;
+      cout<<"2.Delete Voter"<<endl;
+      cout<<"3.Add Candidate"<<endl;
+      cout<<"4.Delete Candidate"<<endl;
+      cout<<"5.Count Votes"<<endl;
+      getoptions();
+     }
+     else{
+      cout<<"Please Check your info and try again";
+      getinput();
      }
     }
+    void getoptions(){
+     int option;
+     cin>>option;
+     if(option==1){
+      AddVoter();
+     }
+     else if(option ==2){
+      DeleteVoter();
+     }
+     else if(option == 3){
+      AddCandidate();
+     }
+     else if(option == 4){
+      DeleteCandidate();
+     }
+     else if(option == 5){
+      CountVotes();
+     }
+     else{
+      cout<<"Please choose valid option";
+      loginCheck();
+     }
+    }
+    void AddVoter(){
+     vDetails newVoter;
+     cout<<"Enter name of new voter";
+     cin>>newVoter.name;
+     cout<<"Enter id of new voter";
+     cin>>newVoter.id;
+     cout<<"Enter password of new voter";
+     cin>>newVoter.password;
+     newVoter.status= false;
+
+     ofstream outfile("voters.txt",ios::app)
+      if(!outfile){
+      cout<<"Error openeing file"<<endl;
+      return;
+     }
+     outFile << newVoter.id << "," << newVoter.name << "," << newVoter.password << "," << newVoter.status << "\n";
+     outFile.close();
+
+     cout << "Voter added successfully!" << endl;
+
       
 };
 void login(){
@@ -64,7 +120,9 @@ void login(){
     voterlogin();
   }
   else if(login1==0){
-    adminlogin();
+    Admin a;
+    a.getinput();
+    
   }
   else{
     cout<<"check your choice and enter again";
