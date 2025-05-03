aa61c0e42ab199e12d173f4cac97f0249531876d
RanjeetSurve4368 <169068143+RanjeetSurve4368@users.noreply.github.com>
2025-05-03 18:53:24 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -89,7 +89,7 @@ vector<aDetails> Admins={
      }
      else{
       cout<<"Please choose valid option";
-      loginCheck();
+      // loginCheck();
      }
     }
     void AddVoter(){
@@ -215,7 +215,7 @@ vector<aDetails> Admins={
       getline(ss, nameStr, ',');
       getline(ss, voteCountStr, ',');
   
-      // Convert string to appropriate typesRanjee
+      // Convert string to appropriate types
       v.id = stoi(idStr);
       v.name = nameStr;
       v.voteCount = stoi(voteCountStr); 
@@ -241,14 +241,266 @@ vector<aDetails> Admins={
        DeleteCandidate();}
      
     }
+    void CountVotes(){
+      cout<<"Please Enter id of Candidate you want to count votes of"<<endl;
+      int idToCount;
+      cin>>idToCount;
+      cout<<"Please Enter name of Candidate you want to count votes of"<<endl;
+      string nameToCount;
+      cin>>nameToCount;
+      vector <cDetails> candidates;
+      ifstream infile("candidates.txt");
+      
+  
+      if (!infile) {
+          cout << "Error opening file!" << endl;
+          return;
+      }
+      string line;
+      bool found=false;
+      while(getline(infile,line)){
+        stringstream ss(line);
+        string idStr,nameStr,voteCountStr;
+        getline(ss,idStr,',');
+        getline(ss,nameStr,',');
+        getline(ss,voteCountStr,',');
+
+        int id=stoi(idStr);
+        int voteCount=stoi(voteCountStr);
+        if (id == idToCount && nameStr == nameToCount) {
+          cout << "Candidate '" << nameStr << "' with ID " << id << " has " << voteCount << " votes." << endl;
+          found = true;
+          break;
+      }
+    }
+    infile.close();
+
+    if (!found) {
+        cout << "Candidate not found with provided details." << endl;
+    }
+
+    getoptions();
+  }
 
 };
+
+class Voter{
+  public:
+  int Vid,Vpassword;
+  string Vname;
+  void login(){
+    cout<<"Please Enter your voter id: "<<endl;
+    // int Vid;
+    cin>>Vid;
+    cout<<"Please enter voter name: "<<endl;
+    // string Vname;
+    cin>>Vname;
+    cout<<"Please enter your password"<<endl;
+    // int Vpassword;
+    cin>>Vpassword;
+    loginCheck(Vid,Vname,Vpassword);
+    
+  }
+  void loginCheck(int Vid,string Vname, int Vpassword){
+    vector <vDetails> voters;
+      ifstream infile("voters.txt");
+      
+  
+      if (!infile) {
+          cout << "Error opening file!" << endl;
+          return;
+      }
+      string line;
+    bool checked = false;
+
+    while (getline(infile, line)) {
+      stringstream ss(line);
+      string idStr, nameStr, passwordStr, statusStr;
+      vDetails v;
+  
+      getline(ss, idStr, ',');
+      getline(ss, nameStr, ',');
+      getline(ss, passwordStr, ',');
+      getline(ss, statusStr, ',');
+  
+      // Convert string to appropriate typesRanjee
+      v.id = stoi(idStr);
+      v.name = nameStr;
+      v.password = stoi(passwordStr);
+      v.status = (statusStr == "1" || statusStr == "true"); // handle true/false or 1/0
+  
+      if (v.id == Vid && v.password == Vpassword && v.name == Vname) {
+          checked = true; 
+          getoption();
+          break;
+      } 
+
+      
+  }
+  }
+  void getoption(){
+    cout<<"Welcome for voting please click 1 to vote "<<endl;
+    int Voption;
+    cin>>Voption;
+    if(Voption==1){
+      getVote();
+    }
+  }
+
+  void getVote(){
+    vector <vDetails> voters;
+      ifstream infile("voters.txt");
+      
+  
+      if (!infile) {
+          cout << "Error opening file!" << endl;
+          return;
+      }
+      string line;
+    bool voted = false;
+
+    while (getline(infile, line)) {
+      stringstream ss(line);
+      string idStr, nameStr, passwordStr, statusStr;
+      vDetails v;
+  
+      getline(ss, idStr, ',');
+      getline(ss, nameStr, ',');
+      getline(ss, passwordStr, ',');
+      getline(ss, statusStr, ',');
+  
+      // Convert string to appropriate typesRanjee
+      v.id = stoi(idStr);
+      v.name = nameStr;
+      v.password = stoi(passwordStr);
+      v.status = (statusStr == "1" || statusStr == "true"); // handle true/false or 1/0
+  
+      if (v.id==Vid && v.name==Vname && v.password==Vpassword) {
+          if(v.status==true){
+            cout<<"You already voted";
+          }
+          else{
+            // cout<<"Helo world";
+            candidateList();
+          }
+          
+      } 
+
+      
+  }
+  }
+  void candidateList(){
+    cout<<"Following are names of candidates"<<endl;
+    vector <cDetails> candidates;
+      ifstream infile("candidates.txt");  
+      if (!infile) {
+          cout << "Error opening file!" << endl;
+          return;
+      }
+      string line;
+      while(getline(infile,line)){
+        stringstream ss(line);
+        string idStr,nameStr,voteCountStr;
+        getline(ss,idStr,',');
+        getline(ss,nameStr,',');
+        getline(ss,voteCountStr,',');
+       
+        cout<<"id: "<<idStr<<" Name: "<<nameStr<<endl;
+    }
+    infile.close();
+    voteForCandidate();
+    
+  }
+  void voteForCandidate(){
+    int chosenId;
+    cout << "Enter the ID of the candidate you want to vote for: ";
+    cin >> chosenId;
+
+    // Step 1: Update vote count in candidates.txt
+    vector<cDetails> candidates;
+    ifstream infile("candidates.txt");
+    string line;
+
+    if (!infile) {
+        cout << "Error opening candidates file!" << endl;
+        return;
+    }
+
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        string idStr, nameStr, voteCountStr;
+        getline(ss, idStr, ',');
+        getline(ss, nameStr, ',');
+        getline(ss, voteCountStr, ',');
+
+        cDetails c;
+        c.id = stoi(idStr);
+        c.name = nameStr;
+        c.voteCount = stoi(voteCountStr);
+
+        if (c.id == chosenId) {
+            c.voteCount += 1;
+        }
+
+        candidates.push_back(c);
+    }
+    infile.close();
+
+    ofstream outfile("candidates.txt");
+    for (auto& c : candidates) {
+        outfile << c.id << "," << c.name << "," << c.voteCount << "\n";
+    }
+    outfile.close();
+
+    // Step 2: Update voter's status in voters.txt
+    vector<vDetails> voters;
+    ifstream vinfile("voters.txt");
+
+    if (!vinfile) {
+        cout << "Error opening voters file!" << endl;
+        return;
+    }
+
+    while (getline(vinfile, line)) {
+        stringstream ss(line);
+        string idStr, nameStr, passwordStr, statusStr;
+        vDetails v;
+
+        getline(ss, idStr, ',');
+        getline(ss, nameStr, ',');
+        getline(ss, passwordStr, ',');
+        getline(ss, statusStr, ',');
+
+        v.id = stoi(idStr);
+        v.name = nameStr;
+        v.password = stoi(passwordStr);
+        v.status = (statusStr == "1" || statusStr == "true");
+
+        if (v.id == Vid && v.name == Vname && v.password == Vpassword) {
+            v.status = true;
+        }
+
+        voters.push_back(v);
+    }
+    vinfile.close();
+
+    ofstream voutfile("voters.txt");
+    for (auto& v : voters) {
+        voutfile << v.id << "," << v.name << "," << v.password << "," << v.status << "\n";
+    }
+    voutfile.close();
+
+    cout << "Vote cast successfully!" << endl;
+}
+};
+
 void login(){
   int login1;
   cout<<"If you want to login as voter please enter 1 and if you want to login as admin please enter 0"<<endl;
   cin>>login1;
   if(login1==1) {
-    voterlogin();
+    Voter a;
+    a.login();
   }
   else if(login1==0){
     Admin a;
